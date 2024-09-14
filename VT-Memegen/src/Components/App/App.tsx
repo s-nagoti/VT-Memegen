35173e4517b5acc94118f4a1775c7d6629153900
Bruce Blake <s.nagoti14@gmail.com>
2024-09-14 00:22:01 -0400

    First Commit
    First Commit.

---
+++ VT-Memegen/src/Components/App/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Cum.
+      </h1>
+      <p className="text-lg text-gray-700">
+        Cum.
+      </p>
+    </div>
+  </div>
+  )
+}
+
+export default App
