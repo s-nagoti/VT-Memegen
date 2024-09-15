eef41b47b37ca108aab2ee1328afff46c8df9792
Ameya Deshmukh <ameyadeshmukh@Ameyas-MacBook-Pro.local>
2024-09-14 21:17:24 -0400

    categories in header

--- VT-Memegen/src/Components/Header/Header.tsx
+++ VT-Memegen/src/Components/Header/Header.tsx
@@ -7,6 +7,14 @@ interface HeaderProps {
   onHomeClick?: () => void;
   onProfileClick?: () => void;
   onCreatePostClick?: () => void;
+
+  //categories
+  onHousingClick?: () => void;
+  onClassesClick?: () => void;
+  onDiningClick?: () => void;
+  onNightLifeClick?: () => void;
+  onSportsClick?: () => void;
+
   showCreatePost?: boolean;
 }
 
@@ -16,6 +24,11 @@ const Header = ({
   onHomeClick,
   onProfileClick,
   onCreatePostClick,
+  onHousingClick,
+  onClassesClick,
+  onDiningClick,
+  onNightLifeClick,
+  onSportsClick,
   showCreatePost,
 }: HeaderProps) => {
   // Extract the username from the email
@@ -24,6 +37,39 @@ const Header = ({
   return (
     <header className="flex justify-between items-center p-5 bg-gray-100 shadow-md">
       <div className="text-2xl font-bold">{title}</div>
+      <div className="flex items-center gap-40">
+        <button
+          className="px-4 py-2 font-medium text-white bg-blue-600 hover:bg-blue-700 rounded"
+          onClick={onHousingClick}
+        >
+          Housing
+        </button>
+        <button
+          className="px-4 py-2 font-medium text-white bg-red-600 hover:bg-red-700 rounded"
+          onClick={onClassesClick}
+        >
+          Classes
+        </button>
+        <button
+          className="px-4 py-2 font-medium text-white bg-yellow-600 hover:bg-yellow-700 rounded"
+          onClick={onDiningClick}
+        >
+          Dining
+        </button>
+        <button
+          className="px-4 py-2 font-medium text-white bg-blue-600 hover:bg-blue-700 rounded"
+          onClick={onNightLifeClick}
+        >
+          NightLife
+        </button>
+        <button
+          className="px-4 py-2 font-medium text-white bg-red-600 hover:bg-red-700 rounded"
+          onClick={onSportsClick}
+        >
+          Sports
+        </button>
+
+      </div>
       <div className="flex items-center gap-4">
         <button
           className="px-4 py-2 font-medium hover:bg-gray-200 rounded"
@@ -32,7 +78,7 @@ const Header = ({
           Home
         </button>
 
- <div className="relative">
+        <div className="relative">
           <button
             className="px-4 py-2 font-medium hover:bg-gray-200 rounded inline-flex items-center"
             onClick={onProfileClick}
@@ -54,15 +100,15 @@ const Header = ({
             </svg>
           </button>
         </div>
-        { showCreatePost &&
-        <button
-          className="px-4 py-2 font-medium text-white bg-red-600 hover:bg-red-700 rounded"
-          onClick={onCreatePostClick}
-        >
-          Create Post
-        </button>
+        {showCreatePost &&
+          <button
+            className="px-4 py-2 font-medium text-white bg-red-600 hover:bg-red-700 rounded"
+            onClick={onCreatePostClick}
+          >
+            Create Post
+          </button>
         }
-       
+
       </div>
     </header>
   );
